85c4296d5303efb392fa8d5fa7a341b612aa1aea
XRFXLP <ajaym6405@gmail.com>
2023-04-17 18:21:05 +0530

    partially done

--- project/content.js
+++ project/content.js
@@ -1,82 +1,122 @@
 const URLPrefix = 'https://www.wikidata.org/wiki/Special:EntityPage'
 
 let wikidataID = null;
-// let container = document.getElementById('mynetwork');
+let graph = {
+  nodes: [],
+  edges: [] 
+}
+const options = {};
+
+
+
+let container = document.getElementById('mynetwork');
 
 // Add button for visualizing
-// let button = document.createElement('button')
-// button.innerText = "Visualize"
-// button.style.backgroundColor = '#111150';
-// button.style.border = 'none';
-// button.style.color = 'white';
-// button.style.padding = '10px 28px';
-// button.style.textAlign = 'center';
-// button.style.textDecoration = 'none';
-// button.style.display = 'inline-block';
-// button.style.fontSize = '14px';
-// button.style.margin = '4px 2px';
-// button.style.cursor = 'pointer';
+let button = document.createElement('button')
+let firstheading = document.getElementById('firstHeading');
+let mwouttext = document.getElementById("mw-content-text");
 
-// button.addEventListener('click', function() {
-//   chrome.runtime.sendMessage({greeting: 'openPopup', action: 'openPopup'});
-//   console.log("button clicked");
-// });
+
+button.innerText = "Visualize"
+button.style.backgroundColor = '#111150';
+button.style.border = 'none';
+button.style.color = 'white';
+button.style.padding = '10px 28px';
+button.style.textAlign = 'center';
+button.style.textDecoration = 'none';
+button.style.display = 'inline-block';
+button.style.fontSize = '14px';
+button.style.margin = '4px 2px';
+button.style.cursor = 'pointer';
 
 
 let div = document.createElement("div")
 div.appendChild(button)
-document.getElementById('firstHeading').appendChild(div )
+firstheading.appendChild(div )
+
+button.addEventListener('click', function() {
+  let mynetwork = document.createElement('div');
+  mynetwork.id = "mynetwork";
+
+  let script = document.createElement("script");
+  script.src = "https://unpkg.com/vis-network/standalone/umd/vis-network.min.js";
+  script.type = 'text/javascript'
+  
+  mwouttext.prepend(mynetwork); 
+  mwouttext.prepend(script);
 
 
-for(let urlElement of document.links){
+  for(let urlElement of document.links){
     if(urlElement.href.startsWith(URLPrefix)){
         wikidataID = urlElement.href.match(/Q\d+/)[0];
         break;
     }
-}
-
-const wikidataURL = `https://www.wikidata.org/wiki/${wikidataID}`
-
-chrome.runtime.onMessage.addListener(receiver);
-
-// Handle the message
-function receiver(request, sender, sendResponse) {
-  if (request.message === "browser action") {    
-    
   }
-}
 
-fetch(wikidataURL)
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(data, 'text/html');
-    let statementGroups = htmlDoc.getElementsByClassName("wikibase-statementgrouplistview");
-    let stuffs = [...statementGroups].map(x=>[...x.getElementsByClassName("wikibase-statementgroupview")]);
+  const wikidataURL = `https://www.wikidata.org/wiki/${wikidataID}`
 
-    let mainNode = {id: 0, label: wikidataURL}
-    let graph = {
-        nodes: [mainNode],
-        edges: [] 
-    }
-    let nodeID = 1;
-    for(let item of stuffs){
-        let predicate = item.children[0].getElementsByTagName('a')[0];
-        let propertyID = predicate['title'], propertyName = predicate.innerText;
+  fetch(wikidataURL)
+    .then(response => response.text())
+    .then(data => {
+  
+  
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(data, 'text/html');
+      let statementGroups = htmlDoc.getElementsByClassName("wikibase-statementgrouplistview");
+      let variables = [...statementGroups].map(x=>[...x.getElementsByClassName("wikibase-statementgroupview")]);
 
-        let values = [...item.children[1].children[0].children].map(x=>x.children[1]
-                                                                        .children[0]
-                                                                        .children[0]
-                                                                        .children[1]
-                                                                        .children[1]
-                                                                        .children[0].innerHTML);
-        console.log(propertyID, propertyName, values)
-        graph['nodes'].push({id: nodeID, label: values})
-        graph['edges'].push({from: 0, to: nodeID, label: propertyName})
-    }
-    const options = {};
-    // const network = new vis.Network(container, graph, options);
-  })
-  .catch(error => {
-    console.log(error);
+      
+      let mainNode = {id: 0, label: wikidataID}
+      graph.nodes.push(mainNode)
+      
+      let nodeID = 1;
+      for(let stuffs of variables)
+        for(let item of stuffs){
+            if(item.children == undefined){
+              continue;
+            }
+              
+
+            let predicate = item.children[0].getElementsByTagName('a');
+
+            if(predicate == undefined)
+                continue;
+
+            predicate = predicate[0];
+
+            let propertyID = predicate['title'], propertyName = predicate.innerText;
+
+            let values = [...item.children[1].children[0].children].map(x=>x.children[1]
+                                                                            .children[0]
+                                                                            .children[0]
+                                                                            .children[1]
+                                                                            .children[1]
+                                                                            .children[0].innerHTML);
+            console.log(propertyID, propertyName, values)
+            graph['nodes'].push({id: nodeID, label: values})
+            graph['edges'].push({from: 0, to: nodeID, label: propertyName})
+            nodeID += 1;
+        }
+      console.log(graph);
+
+      let grapher = document.createElement("script");
+      grapher.textContent = `
+        let data = ${JSON.stringify(graph)};
+        let options = {
+              nodes: {
+                shape:"dot"
+              }
+            }
+        let mynetwork = document.getElementById('mynetwork');
+        let network = new vis.Network(mynetwork, data, options);
+        console.log(mynetwork);
+      `
+      grapher.type = 'text/javascript'
+      mwouttext.appendChild(grapher); 
+
+      return data
+    })
+    .catch(error => {
+      console.log(error);
   });
+});
--- project/grapher.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("message", function(event) {
-    // Check that message came from the extension
-    if (event.source === window && event.data.greeting) {
-      // Do something with the data
-      console.log(event.data.greeting);
-    }
-  });
-  
